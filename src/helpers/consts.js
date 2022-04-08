857ec44b4761d5d391856ed94d602472908646a9
azimkg <azeldenbaev@gmail.com>
2022-04-08 11:31:17 +0600

    add

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,6 +1,6 @@
-export const PRODUCTS_API = "http://localhost:8000/products";
+export const PRODUCTS_API = "https://deploy-js17.herokuapp.com/products";
 
-export const BRANDS_API = "http://localhost:8000/brands";
+export const BRANDS_API = "https://deploy-js17.herokuapp.com/brands";
 
 export const ADMIN_EMAIL = "begimay@gmail.com";
 // const brands = [
